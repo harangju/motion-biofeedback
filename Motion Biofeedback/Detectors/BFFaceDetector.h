3bc14a0dfed91cd1905f43028e49868db5f9e7c0
Harang Ju <wngkfkd94@gmail.com>
2014-03-02 00:58:37 -0500

    trying CIImage face detection

---
+++ Motion Biofeedback/Detectors/BFFaceDetector.h
@@ -0,0 +1,13 @@
+//
+//  BFFaceDetector.h
+//  Motion Biofeedback
+//
+//  Created by Harang Ju on 3/2/14.
+//  Copyright (c) 2014 Harang Ju. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BFFaceDetector : NSObject
+
+@end
---
+++ Motion Biofeedback/Detectors/BFFaceDetector.m
@@ -0,0 +1,13 @@
+//
+//  BFFaceDetector.m
+//  Motion Biofeedback
+//
+//  Created by Harang Ju on 3/2/14.
+//  Copyright (c) 2014 Harang Ju. All rights reserved.
+//
+
+#import "BFFaceDetector.h"
+
+@implementation BFFaceDetector
+
+@end
