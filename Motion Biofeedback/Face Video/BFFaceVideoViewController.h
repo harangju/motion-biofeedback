8c54733787abcec3404c0f572a2779b941eb2ce7
Harang Ju <wngkfkd94@gmail.com>
2014-03-08 18:44:32 -0500

    added new view controller for iPad

---
+++ Motion Biofeedback/Face Video/BFFaceVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  BFFaceVideoViewController.h
+//  Motion Biofeedback
+//
+//  Created by Harang Ju on 3/8/14.
+//  Copyright (c) 2014 Harang Ju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BFFaceVideoViewController : UIViewController
+
+@end
